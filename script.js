b4be0db04ec03d6d27e13008c8e4df6dea2b5370
Romain Roussel <romain.roussel29@gmail.com>
2026-05-07 14:15:01 +0200

    feat(css+js) : Added slider interaction

--- script.js
+++ script.js
@@ -13,3 +13,30 @@ titles.forEach(title => {
         }
     });
 });
+
+const left = document.querySelector('#left');
+const right = document.querySelector('#right');
+const slider = document.querySelector('.languages-content');
+const nbElements = slider.childElementCount;
+let i = 0;
+right.addEventListener('click', () => {
+    if(i < nbElements - 4)
+        i++;
+    else
+        i = 0;
+
+    updateSlider(i);
+});
+
+left.addEventListener('click', () => {
+    if(i > 0)
+        i--;
+    else
+        i = nbElements - 4;
+
+    updateSlider(i);
+});
+
+function updateSlider(index) {
+    slider.style.transform = 'translateX(' + (-index * (228 + 16)) + 'px)';
+}
